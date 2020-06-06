58f0b71dcc63f859dead83e49b1d6c5d2d9bb777
unknown <swapnil.ahirrao@mobisoftinfotech.com>
2020-06-06 10:19:09 +0530

    issue resolved

--- src/app/storage-service.service.ts
+++ src/app/storage-service.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
 import * as CryptoJS from 'node_modules/crypto-browserify';
-const SecureStorage = require('secure-web-storage');
-const SECRET_KEY = 'secret_key';
+// const SecureStorage = require('secure-web-storage');
+// const SECRET_KEY = 'secret_key';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageServiceService {
 
-  constructor() { }
-  public secureStorage = new SecureStorage(localStorage, {
-    // Encrypt the localstorage data
-    encrypt: function encrypt(data) {
-        data = CryptoJS.AES.encrypt(data, SECRET_KEY);
-        data = data.toString();
-        return data;
-    },
-    // Decrypt the encrypted data
-    decrypt: function decrypt(data) {
-        data = CryptoJS.AES.decrypt(data, SECRET_KEY);
-        data = data.toString(CryptoJS.enc.Utf8);
-        return data;
-    }
-    });
+  // constructor() { }
+  // public secureStorage = new SecureStorage(localStorage, {
+  //   // Encrypt the localstorage data
+  //   encrypt: function encrypt(data) {
+  //       data = CryptoJS.AES.encrypt(data, SECRET_KEY);
+  //       data = data.toString();
+  //       return data;
+  //   },
+  //   // Decrypt the encrypted data
+  //   decrypt: function decrypt(data) {
+  //       data = CryptoJS.AES.decrypt(data, SECRET_KEY);
+  //       data = data.toString(CryptoJS.enc.Utf8);
+  //       return data;
+  //   }
+  //   });
 }
